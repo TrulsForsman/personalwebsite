b36d5fc7c0294a0842ad51ab276226bd2b94af8d
Truls Forsman <Truls.Forsman@protonmail.com>
2023-02-13 17:53:56 +0100

    Split up the code a bit. Might change in the future.

---
+++ includes/cuvi.php
@@ -0,0 +1,6 @@
+<div class="headerline2">
+    <div class="third_section">
+        <p class="header">Curriculum vitae</p>
+        <hr />
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -2,26 +2,15 @@
 </head>
 <body>
     <?php require_once('includes/navbar.php') ?>
-        <?php require_once('includes/content.php') ?>
+        <?php require_once('includes/tfsign.php') ?>
     <div class="content">
     <div class="second_section">
     <div class="headerline">
     <p class="header">About me</p>
     <hr />
     </div>
-    <div class="second_sectionflex">
-    <p class="txt" id="txt1">Hello! My name is Truls Forsman. I come from southern Sweden and have a great ambition to code. My interest in coding began 6 years ago when I started learning Python and Java. </p>
-    <p class="txt" id="txt2">Today I focus on full-stack WebAplications. From the user clicking on the website to handling the website’s database.</p>
-    <p class="txt" id="txt3">On my journey, I have learned a lot. I have met and worked on several languages including Python, Java, C++, and Rust until I began making websites and fell in love with PHP and React.</p>
-    <p class="txt" id="txt4">For me, coding is much more than typing letters that are transmitted to ones and zeroes. I feel like coding has helped me a lot in my life and made me develop as a person. </p>
-    </div>
-    </div>
-    <div class="headerline2">
-    <div class="third_section">
-        <p class="header">Curriculum vitae</p>
-        <hr />
-        </div>
-    </div>
+    <?php require_once('includes/ssc.php') ?>
+    <?php require_once('includes/cuvi.php') ?>
     </div>
 </body>
 </html>
